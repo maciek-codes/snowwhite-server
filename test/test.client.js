c658aa67fb8fba57f777c0bf35a6577c2f41716f
Maciej Kumorek <mkumorek@gmail.com>
2013-11-07 12:46:01 +0000

    Added Mocha testing framework and basic test

---
+++ test/test.client.js
@@ -0,0 +1,21 @@
+var assert = require('assert'),
+	Client = require('./../client.js');
+
+
+describe("Client", function () {
+	
+	var testName = "129.123.023.001:3000";
+	var type = "mobile";
+
+	var testClient = new Client(testName, type);
+
+	it('should have name', function(){
+
+		assert.equal(testName, testClient.getName());
+	});
+
+	it('should have type', function() {
+		assert.equal(type, testClient.getClientType());
+	})
+
+});
