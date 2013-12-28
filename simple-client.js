2439cc994bf63db6a7085312d5b4252b05a4c15f
ellie ford <hf0486@my.bristol.ac.uk>
2013-12-28 23:33:55 +0000

    updated simple client to send throw and catch gestures

--- simple-client.js
+++ simple-client.js
@@ -1,15 +1,19 @@
 var net = require('net');
+var keypress = require('keypress');
+keypress(process.stdin);
 
-var host = '172.21.213.212';
+var host = '192.168.3.1';
 var port = 1337;
 
+var stdin = process.openStdin(); 
+require('tty').setRawMode(true); 
 var client = new net.Socket();
 
 client.connect(port, host, function () {
 
     console.log('CONNECTED TO: ' + host + ':' + port);
 
-    client.write('{ "dest":0, "msg": {"type" : "hello", "client" : "mobile"} }');
+    client.write('{ "dest":0, "msg": {"type" : "hello", "client" : "mobile", "platform" : "Android 4.1"} }');
 
 });
 
@@ -18,8 +22,19 @@ client.connect(port, host, function () {
 client.on('data', function (data) {
     
     console.log('DATA: ' + data);
+     });
     
+
+   
+
+stdin.on('keypress', function (chunk, key) {
+  process.stdout.write('Get Chunk: ' + chunk + '\n');
+  if (key && key.ctrl && key.name == 'c') process.exit();
+  if( key.name == 'c')  client.write('{ "dest":0, "msg": {"type" : "catch", "sender" : "1"} }');
+  if( key.name == 't')  client.write('{ "dest":0, "msg": {"type" : "throw", "recipiant" : "2"} }');
+
 });
+
 
 // Add a 'close' event handler for the client socket
 client.on('close', function() {
