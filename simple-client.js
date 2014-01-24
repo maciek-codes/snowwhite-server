1e73a08c143b9df0ab884d532f6c66ee7f2ddbe6
ellie ford <hf0486@my.bristol.ac.uk>
2014-01-24 13:23:14 +0000

    updated

--- simple-client.js
+++ simple-client.js
@@ -8,12 +8,13 @@ var port = 1337;
 var stdin = process.openStdin(); 
 require('tty').setRawMode(true); 
 var client = new net.Socket();
+try{
 
 client.connect(port, host, function () {
 
     console.log('CONNECTED TO: ' + host + ':' + port);
 
-    client.write('{ "dest":0, "msg": {"type" : "hello", "client" : "mobile", "platform" : "Android 4.1"} }');
+    client.write('{ "dest":0, "msg": {"type" : "hello", "client" : "mobile", "platform" : "Android"} }');
 
 });
 
@@ -30,8 +31,8 @@ client.on('data', function (data) {
 stdin.on('keypress', function (chunk, key) {
   process.stdout.write('Get Chunk: ' + chunk + '\n');
   if (key && key.ctrl && key.name == 'c') process.exit();
-  if( key.name == 'c')  client.write('{ "dest":0, "msg": {"type" : "catch", "sender" : "1"} }');
-  if( key.name == 't')  client.write('{ "dest":0, "msg": {"type" : "throw", "recipiant" : "2"} }');
+  if( key.name == 'c')  client.write('{ "dest":0, "msg": {"type" : "catch", "sender" : 2} }');
+  if( key.name == 't')  client.write('{ "dest":0, "msg": {"type" : "throw", "recipient" : 1} }');
 
 });
 
@@ -39,4 +40,7 @@ stdin.on('keypress', function (chunk, key) {
 // Add a 'close' event handler for the client socket
 client.on('close', function() {
     console.log('Connection closed');
-});
+});}catch(err)
+		{
+			console.log(err);
+		}
