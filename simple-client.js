f253887ad89e9ffd30e60e8b05533753c0079830
hf0486 <hf0486@my.bristol.ac.uk>
2014-03-06 15:57:50 +0000

    untested simple client sending speed and ballid

--- simple-client.js
+++ simple-client.js
@@ -9,8 +9,15 @@ var stdin = process.openStdin();
 require('tty').setRawMode(true); 
 var client = new net.Socket();
 var ballId_caught;
+var sender;
+var players;
+var myId;
+var ballId = 1;
+var powerUps = [];
+
+var alreadyAppliedPowerUp = 0;
 try{
-var MacId = process.argv[2];
+var MacId =  process.argv[2];
 client.connect(port, host, function () {
 
     console.log('CONNECTED TO: ' + host + ':' + port);
@@ -29,10 +36,14 @@ client.on('data', function (data) {
 	try {
 			var originalMessage = JSON.parse(data);
 			var message = originalMessage.msg;
-			if (message.type == "incoming" || message.type == "started") {
+			if (message.type == "incoming" || message.type == "started" ) {
 				ballId_caught = message.ballId;
+				ballId = message.ballId;
+				sender =message.sender;
 				console.log('test id ' + ballId_caught);
-
+			}
+			else if(message.type =="newBall"){
+					ballId = message.ballId;
 			}
 	} catch(err) {
 		console.log(err);
@@ -46,11 +57,21 @@ stdin.on('keypress', function (chunk, key) {
   process.stdout.write('Get Chunk: ' + chunk + '\n');
   if (key && key.ctrl && key.name == 'c') process.exit();
   if( key.name == 'c') {
-	client.write('{ "dest":0, "msg": {"type" : "catch", "ballId" : '+ballId_caught + ', "sender" : 2} }');
+	console.log('   < Sending message >');
+			        console.log('   { "dest":0, "msg": {"type" : "catch", "ballId" : '+ ballId + ', "sender" : ' + sender + '} }');
+			        client.write('{ "dest":0, "msg": {"type" : "catch", "ballId" : '+ballId + ', "sender" : ' + sender + '} }');
+			        alreadyAppliedPowerUp = data.indexOf('appliedPowerUps');
   }
   if( key.name == 't'){
-	client.write('{ "dest":0, "msg": {"type" : "throw", "ballId" : '+ballId_caught+', "recipient" : 1} }');
-	console.log('throw ball id ' + ballId_caught);
+	 console.log('   < Sending message >');
+        			    var powerUpMsg = "";
+        			    if(powerUps.length > 0 && alreadyAppliedPowerUp == -1) {
+        			         powerUpMsg = ', "appliedPowerUps": { "type": "' + powerUps.pop() + '", "strength" : 1 } ';
+        			    }
+                  var speedMsg = ', "speed":' + Math.ceil(Math.random() * 100);
+                  var throwTo = 1;
+        			    console.log('   { "dest":0, "msg": {"type" : "throw", "ballId" : '+ ballId +', "recipient" : ' + throwTo + speedMsg + powerUpMsg + '} }');
+        			    client.write('{ "dest":0, "msg": {"type" : "throw", "ballId" : '+ ballId +', "recipient" : ' + throwTo + speedMsg + powerUpMsg + '} }');
 
 	}
 
